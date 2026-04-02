1fa8f22d730e4d0c62f500bd6bc5599c331a5a25
Leon Si <leon@leonsilicon.com>
2026-04-02 17:32:24 -0400

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
 pub trait FrontmostApp: Debug {
-    fn set_frontmost(&mut self, new_value: &str);
+    fn set_frontmost(&mut self, new_value: Option<String>);
     fn update(&mut self);
 }
